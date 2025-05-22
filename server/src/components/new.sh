4e81fd8b91ab728f2c0fe003d3b27ee27f9fae09
Dan Habot <dan.habot@gmail.com>
2025-05-22 18:52:18 -0400

    fix new script

--- server/src/components/new.sh
+++ server/src/components/new.sh
@@ -21,7 +21,7 @@ EOT
 
 cat <<EOF > "$1/stories.tsx"
 import type { Meta, StoryObj } from '@storybook/react';
-import { ${1} } from './${1}';
+import { ${1} } from './component';
 
 // More on how to set up stories at:
 // https://storybook.js.org/docs/writing-stories#default-export
