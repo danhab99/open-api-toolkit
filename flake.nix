1b870027164285ff1059593e2452583f25681c3c
Dan Habot <dan.habot@gmail.com>
2025-05-24 01:15:44 -0400

    install prisma

--- flake.nix
+++ flake.nix
@@ -23,6 +23,13 @@
               prettierd
             ];
 
+            PRISMA_QUERY_ENGINE_LIBRARY =
+              "${pkgs.prisma-engines}/lib/libquery_engine.node";
+            PRISMA_QUERY_ENGINE_BINARY =
+              "${pkgs.prisma-engines}/bin/query-engine";
+            PRISMA_SCHEMA_ENGINE_BINARY =
+              "${pkgs.prisma-engines}/bin/schema-engine";
+
             shellHook = ''
             zsh
             '';
