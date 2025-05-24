c73dd1a3e0804db6200e065a423f3a99c7531dd3
Dan Habot <dan.habot@gmail.com>
2025-05-24 00:30:05 -0400

    created types and examples

---
+++ server/types.ts
@@ -0,0 +1,38 @@
+
+export type ID = string;
+
+export type Identifier = {
+  name: string;
+  userDescription: string;
+  aiDescription: string;
+}
+
+export type ConfigArg = Identifier & {
+  id: ID;
+  type: "string" | "number"
+};
+
+export type Resource = Identifier & {
+
+};
+
+export type Tool = Identifier & {
+  arguments: ConfigArg[]
+};
+
+export type McpConnectionDefinition = Identifier & {
+  id: ID;
+  configurationArguments: ConfigArg[];
+  resources: Resource[];
+  tools: Tool[];
+};
+
+export type Config = ConfigArg & {
+  configId: ConfigArg["id"];
+  value: "string" | "number";
+};
+
+export type McpConnection = Identifier & {
+  mcp: McpConnectionDefinition;
+  config: Config[];
+};
