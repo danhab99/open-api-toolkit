6a28dc32dfe94fc3beb5f9a313721c58bec1673e
Dan Habot <dan.habot@gmail.com>
2025-05-24 00:19:36 -0400

    created config editor

---
+++ server/src/components/EditConnection/component.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Config, McpConnection } from "../../../types";
+import { ConfigInput } from "../ConfigInput";
+import { Button } from "../ui/button";
+
+export type EditConnectionProps = {
+  mcp: McpConnection;
+  onConfigsChanged: (c: Config[]) => void;
+};
+
+export function EditConnection(props: EditConnectionProps) {
+  const [configs, setConfigs] = useState(props.mcp.config);
+
+  return (
+    <>
+      <h1 className="text-2xl py-2">{props.mcp.name}</h1>
+      <p className="text-gray-500 pb-4">{props.mcp.userDescription}</p>
+      {configs.map((config, i) => (
+        <div key={i} className="w-full py-1">
+          <ConfigInput
+            config={config}
+            onConfigChange={(c) =>
+              setConfigs((prev) => [
+                ...prev.slice(0, i),
+                {
+                  ...prev[i],
+                  value: c,
+                },
+                ...prev.slice(i),
+              ])
+            }
+          />
+        </div>
+      ))}
+
+      <div className="flex flex-row gap-2 py-2">
+        <Button>Save</Button>
+        <Button variant="outline">Cancel</Button>
+      </div>
+    </>
+  );
+}
---
+++ server/src/components/EditConnection/stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { EditConnection } from "./component";
+import { exampleMcpConnection } from "../../../example_mcp";
+
+// More on how to set up stories at:
+// https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof EditConnection> = {
+  title: "EditConnection",
+  component: EditConnection,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Test: Story = {
+  args: {
+    mcp: exampleMcpConnection,
+  },
+};
