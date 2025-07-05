d7b217bb05f861f76584a9c8400fce3476fcd0dd
Dan Habot <dan.habot@gmail.com>
2025-07-05 17:20:42 -0400

    added list calendars tool

---
+++ connections/google/src/calendar/list_calendars.ts
@@ -0,0 +1,43 @@
+import { Tool } from "open-api-connection-types";
+import { getCalendar } from "../lib";
+
+export const listGoogleCalendarsWithServiceAccount: Tool = {
+  name: "listGoogleCalendarsWithServiceAccount",
+  userDescription:
+    "View upcoming events from a Google Calendar using a service account.",
+  aiDescription:
+    "Retrieve calendar events using a configured Google service account with access to a specific calendar.",
+  arguments: [],
+  async handler(config, args) {
+    const calendar = getCalendar(config);
+
+    try {
+      const res = await calendar.calendarList.list();
+
+      return {
+        results: {
+          calendars:
+            res.data.items?.map((cal) => ({
+              calendarId: cal.id,
+              color: cal.colorId,
+              primary: cal.primary,
+              description: cal.description,
+              summary: cal.summary,
+              timeZone: cal.timeZone,
+            })) ?? [],
+        },
+        log: {
+          message: `Fetched ${res.data.items?.length || 0} calendars.`,
+        },
+      };
+    } catch (error) {
+      return {
+        results: {},
+        log: {
+          message: "Failed to fetch calendar events",
+          data: error,
+        },
+      };
+    }
+  },
+};
--- connections/google/src/index.ts
+++ connections/google/src/index.ts
@@ -1,23 +1,25 @@
 import { OpenAPIConnectionDefinition } from "open-api-connection-types";
 import { createGoogleCalendarEventsWithServiceAccount } from "./calendar/create_calendar";
-import { deleteGoogleCalendarEventWithServiceAccount } from "./calendar/delete_calendar";
-import { editGoogleCalendarEventWithServiceAccount } from "./calendar/edit_calendar";
 import { createGoogleContact } from "./contacts/create_contact";
-import { deleteGoogleContact } from "./contacts/delete_contact";
-import { updateGoogleContact } from "./contacts/update_contact";
 import { createGoogleDriveFile } from "./drive/create_doc";
+import { createGoogleTask } from "./tasks/create_task";
+import { deleteGoogleCalendarEventWithServiceAccount } from "./calendar/delete_calendar";
+import { deleteGoogleContact } from "./contacts/delete_contact";
 import { deleteGoogleDriveFile } from "./drive/delete_doc";
-import { listGoogleDriveFiles } from "./drive/list_drive";
-import { moveGoogleDriveFile } from "./drive/move_doc";
-import { updateGoogleDriveFile } from "./drive/update_doc";
+import { deleteGoogleTask } from "./tasks/delete_task";
+import { editGoogleCalendarEventWithServiceAccount } from "./calendar/edit_calendar";
 import { getGmailThreadWithServiceAccount } from "./gmail/get_emails";
 import { listGmailMessagesWithServiceAccount } from "./gmail/list_gmail";
+import { listGoogleCalendarsWithServiceAccount } from "./calendar/list_calendars";
+import { listGoogleDriveFiles } from "./drive/list_drive";
+import { listGoogleTasks } from "./tasks/list_tasks";
+import { moveGoogleDriveFile } from "./drive/move_doc";
 import { searchGmailMessagesWithServiceAccount } from "./gmail/search_emails";
 import { sendGmailMessageWithServiceAccount } from "./gmail/send_email";
-import { createGoogleTask } from "./tasks/create_task";
-import { deleteGoogleTask } from "./tasks/delete_task";
-import { listGoogleTasks } from "./tasks/list_tasks";
+import { updateGoogleContact } from "./contacts/update_contact";
+import { updateGoogleDriveFile } from "./drive/update_doc";
 import { updateGoogleTask } from "./tasks/update_task";
+import { viewGoogleCalendarEventsWithServiceAccount } from "./calendar/view_calendar";
 
 export const Connection: OpenAPIConnectionDefinition = {
   id: "google",
@@ -43,22 +45,24 @@ export const Connection: OpenAPIConnectionDefinition = {
 
 export const Tools = [
   createGoogleCalendarEventsWithServiceAccount,
-  deleteGoogleCalendarEventWithServiceAccount,
-  editGoogleCalendarEventWithServiceAccount,
   createGoogleContact,
-  deleteGoogleContact,
-  updateGoogleContact,
   createGoogleDriveFile,
+  createGoogleTask,
+  deleteGoogleCalendarEventWithServiceAccount,
+  deleteGoogleContact,
   deleteGoogleDriveFile,
-  listGoogleDriveFiles,
-  moveGoogleDriveFile,
-  updateGoogleDriveFile,
+  deleteGoogleTask,
+  editGoogleCalendarEventWithServiceAccount,
   getGmailThreadWithServiceAccount,
   listGmailMessagesWithServiceAccount,
+  listGoogleCalendarsWithServiceAccount,
+  listGoogleDriveFiles,
+  listGoogleTasks,
+  moveGoogleDriveFile,
   searchGmailMessagesWithServiceAccount,
   sendGmailMessageWithServiceAccount,
-  createGoogleTask,
-  deleteGoogleTask,
-  listGoogleTasks,
+  updateGoogleContact,
+  updateGoogleDriveFile,
   updateGoogleTask,
+  viewGoogleCalendarEventsWithServiceAccount,
 ];
