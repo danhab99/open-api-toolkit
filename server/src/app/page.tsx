8add99ab60088596d0e0be03f087a22d834755f7
Dan Habot <dan.habot@gmail.com>
2025-08-10 23:57:53 -0400

    type fix

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
         </Link>
       }
     >
-      <ListMyConnections mcps={myConnections} />
+      <ListMyConnections connections={myConnections} />
     </Frame>
   );
 }
