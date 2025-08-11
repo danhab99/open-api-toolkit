1e337f5695e37da5fef5568695598912d92431ae
Dan Habot <dan.habot@gmail.com>
2025-08-10 23:58:14 -0400

    fix names

--- server/src/lib/tool.ts
+++ server/src/lib/tool.ts
@@ -5,55 +5,47 @@ import { db } from "open-api-db";
 
 export async function executeToolCall(
   connectionID: number,
-  toolName: string,
+  toolId: string,
   args: KVP,
 ) {
-  console.log("!!!", { connectionID });
   const connectionInfo = await getMyConnection({
     id: connectionID,
   });
   if (!connectionInfo) {
     throw "connection not found";
   }
-  console.log("!!!", { connectionInfo });
 
   const tools = await getTools(connectionInfo.def.id);
-  const tool = tools.find((t) => t.name === toolName);
+  const tool = tools.find((t) => t.id == toolId);
   if (!tool) {
     throw "tool not found";
   }
-  console.log("!!!", { tools, tool, toolName });
 
   const config: KVP = connectionInfo.config.reduce(
     (configObj, c) => ({
       ...configObj,
-      [c.name]: c.value,
+      [c.id]: c.value,
     }),
     {},
   );
-  console.log("!!!", { config });
-
-  console.log("Executing tool call", { connectionID, toolName, config });
 
   try {
     const results = await tool.handler(config, args);
-    console.log("!!!", { results });
 
     await db.auditLog.create({
       data: {
         connectionID: connectionInfo.db!.id,
-        message: results.log?.message ?? `${connectionInfo.name} just ran`,
+        message: results.log?.message ?? `${connectionInfo.displayName} just ran`,
         data: JSON.stringify(results.log?.data ?? {}),
       },
     });
 
     return results;
   } catch (e: any) {
-    console.log("!!!", { e });
     await db.auditLog.create({
       data: {
         connectionID: connectionInfo.db!.id,
-        message: `${connectionInfo.name} threw an exception`,
+        message: `${connectionInfo.displayName} threw an exception`,
         data: JSON.stringify(e ?? {}),
       },
     });
