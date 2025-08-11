0afa527e6883b85dff8bc6c1ce1095749f0068eb
Dan Habot <dan.habot@gmail.com>
2025-08-10 23:58:04 -0400

    redirect to new

--- server/src/app/page.tsx
+++ server/src/app/page.tsx
@@ -3,9 +3,14 @@ import { ListMyConnections } from "@/components/ListMyConnections";
 import { Button } from "@/components/ui/button";
 import { getMyConnections } from "@/lib/connection";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const myConnections = await getMyConnections();
+
+  if (myConnections.length == 0) {
+    redirect("/new");
+  }
 
   return (
     <Frame
