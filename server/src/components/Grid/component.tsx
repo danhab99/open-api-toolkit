f7abffb8c39f3130e44398b74dd1245ddbfcf167
Dan Habot <dan.habot@gmail.com>
2026-01-21 00:04:10 -0500

    set column count

--- server/src/components/Grid/component.tsx
+++ server/src/components/Grid/component.tsx
@@ -5,5 +5,5 @@ export type GridProps = {
 };
 
 export function Grid(props: React.PropsWithChildren<GridProps>) {
-  return <div className="grid grid-cols-3 gap-4">{props.children}</div>;
+  return <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">{props.children}</div>;
 }
