058c1eb56093b0ae2bb47e18d3323f7aa582a9c7
Dan Habot <dan.habot@gmail.com>
2026-01-20 23:13:53 -0500

    fixed building

--- flake.nix
+++ flake.nix
@@ -12,31 +12,6 @@
           config.allowUnfree = true;
         };
 
-        # Just list the project directories
-        projectPaths = [
-          ./server
-          # ./connections/google
-          # ./prisma
-        ];
-
-        # Turn each project path into a nodeDeps derivation
-        nodeDeps = map (projPath:
-          let
-            pkgJson = builtins.fromJSON (builtins.readFile (projPath + "/package.json"));
-          in
-            pkgs.mkYarnPackage {
-              pname = "${pkgJson.name}-deps";
-              version = pkgJson.version or "1.0.0";
-              src = projPath;
-              packageJSON = projPath + "/package.json";
-              yarnLock   = projPath + "/yarn.lock";
-              # optional: add yarnNix = projPath + "/yarn.nix"; if you pre-generate one
-            }
-        ) projectPaths;
-
-        # Join all node_modules into a single NODE_PATH
-        nodePath = pkgs.lib.concatStringsSep ":" (map (nd: "${nd}/lib/node_modules") nodeDeps);
-
       in {
         devShells = {
           default = pkgs.mkShell {
@@ -55,15 +30,11 @@
             PRISMA_SCHEMA_ENGINE_BINARY =
               "${pkgs.prisma-engines}/bin/schema-engine";
 
-            # All node_modules from projects
-            NODE_PATH = nodePath;
-
             # Database path
             DATABASE_PATH = "file:/home/dan/Documents/node/mcp-server/db";
 
             shellHook = ''
               export DATABASE_PATH="file:$(dirname $(realpath $(git rev-parse --git-dir)))/db"
-              zsh
             '';
           };
         };
--- server/next.config.ts
+++ server/next.config.ts
@@ -1,12 +1,11 @@
-const withTM = require("next-transpile-modules")([
-  "@open-api-connection/google",
-  "@open-api-connection/jira",
-  "@open-api-connection/rss",
-]);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["@open-api-connection/google", "@open-api-connection/jira", "@open-api-connection/rss"],
+  transpilePackages: [
+    "@open-api-connection/google",
+    "@open-api-connection/jira",
+    "@open-api-connection/rss",
+    "@open-api-connection/monday"
+  ],
 };
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
